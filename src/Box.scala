aec6320217af03aa9dc42b36616c7803a4ebe6bd
Ralph Collett <ralph.collett@gmail.com>
2015-04-01 21:14:38 +0100

    Monoids

--- src/Box.scala
+++ src/Box.scala
@@ -10,5 +10,39 @@ object Box {
   def flatMap[A, B](f: A => Box[B]) = (a: Box[A]) => f(a.value)
 
   // applicative
-  def apply[A, B](f: Box[A => B]) = (a: Box[A]) => Box(f.value(a.value))
+  def _apply[A, B](f: Box[A => B]) = (a: Box[A]) => Box(f.value(a.value))
 }
+
+trait Monoid[T] {
+  def zero: T
+  def op: (T, T) => T
+}
+
+object Monoid {
+  
+  def zeroRule[T](t: T, monoid: Monoid[T]) = monoid.op(t, monoid.zero) == t
+
+  def associativeRule[T](t1: T, t2: T,  t3: T, monoid: Monoid[T]) =
+    monoid.op(monoid.op(t1, t2), t3) == monoid.op(t1, monoid.op(t2, t3))
+}
+
+class Addition extends Monoid[Int] {
+  override def zero: Int = 0
+
+  override def op: (Int, Int) => Int = _ + _
+}
+
+class StringConcatenate extends Monoid[String] {
+
+  override def zero: String = ""
+
+  override def op: (String, String) => String = _ + _
+}
+
+class Multiplication extends Monoid[Int] {
+
+  override def zero: Int = 1
+
+  override def op: (Int, Int) => Int = _ * _
+}
+
